c8208490cfefce8e31c2e71c117b9c4d3537b38a
DavorKom <komljenovic.davor@gmail.com>
2019-12-11 08:10:21 +0100

    init commit

---
+++ database/migrations/2019_12_10_130146_create_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('project_manager_id')->unsigned()->nullable();
+            $table->bigInteger('team_lead_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('project_manager_id')->references('id')->on('users');
+            $table->foreign('team_lead_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
